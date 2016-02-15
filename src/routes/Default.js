5e1b4b0e4bef0b475bc951da692a8879c8e17aee
Jesse Harlin <Jesse Harlin>
2016-02-15 08:44:03 -0600

    update structor

--- src/routes/Default.js
+++ src/routes/Default.js
@@ -7,10 +7,8 @@ import '../assets/css/main.css';
 
 import Row from '../components/FlexboxGrid/Row.jsx';
 import Col from '../components/FlexboxGrid/Col.jsx';
-import Box from '../components/FlexboxGrid/Box.jsx';
-import { RaisedButton } from 'material-ui';
-import { TextField } from 'material-ui';
-import TableX from '../components/MaterialUITable/TableX.jsx';
+import TodoForm from '../components/DemoGroup/TodoForm.jsx';
+import TodoList from '../components/DemoGroup/TodoList.jsx';
 
 
 class Default extends Component {
@@ -20,26 +18,9 @@ class Default extends Component {
             <div>
                 <div params={ this.props.params }>
                     <Row className="row center-xs" params={ this.props.params }>
-                        <Col className="col-xs-6" params={ this.props.params }>
-                        <Box params={ this.props.params }>
-                            <TextField hintText="Hint text" params={ this.props.params }></TextField>
-                            <RaisedButton label="Default"
-                                          primary={ true }
-                                          disabled={ false }
-                                          params={ this.props.params }></RaisedButton>
-                        </Box>
-                        <Box params={ this.props.params }>
-                            <TableX fixedHeader={ true }
-                                    fixedFooter={ true }
-                                    stripedRows={ false }
-                                    showRowHover={ false }
-                                    selectable={ true }
-                                    multiSelectable={ true }
-                                    enableSelectAll={ true }
-                                    deselectOnClickaway={ true }
-                                    height="300px"
-                                    params={ this.props.params }></TableX>
-                        </Box>
+                        <Col className="col-xs-8" params={ this.props.params }>
+                          <TodoForm params={ this.props.params }></TodoForm>
+                          <TodoList params={ this.props.params }></TodoList>
                         </Col>
                     </Row>
                 </div>
@@ -49,4 +30,3 @@ class Default extends Component {
 }
 
 export default Default;
-
